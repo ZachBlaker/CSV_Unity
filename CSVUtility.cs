bf8df9b45f06eda887f88dbe9c795daaa9e700b7
ZachBlaker <71710445+ZachBlaker@users.noreply.github.com>
2022-04-06 22:10:12 +0200

    Update CSVUtility.cs

--- CSVUtility.cs
+++ CSVUtility.cs
@@ -3,65 +3,67 @@ using System.Text.RegularExpressions;
 using System.IO;
 using UnityEditor;
 
-
-public static class CSVUtility
+namespace SpreadSheets
 {
-    const string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
-    const string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
-
-    public static CSV GenerateCSVFromFile(string fullPath)
+    public static class CSVUtility
     {
-        Debug.Log("Loading CSV from path " + fullPath);
-        if (!File.Exists(fullPath))
-            throw new System.Exception("File does not exist at path " + fullPath);
+        const string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+        const string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
 
-        FileStream fileStream = new FileStream(fullPath, FileMode.Open, FileAccess.ReadWrite);
-        StreamReader read = new StreamReader(fileStream);
-
-        string data = read.ReadToEnd();
-        read.Close();
-        fileStream.Close();
-        return GenerateCSVFromTextData(data);
-    }
-
-    public static CSV GenerateCSVFromTextData(string data)
-    {
-        Debug.Log("Populating CSV from data");
-
-        string[] rowData = Regex.Split(data, LINE_SPLIT_RE);
-
-        int rowCount = rowData.Length -1;   //Skip over last row as it's created due to the final linebreak for end of file
-        int columnCount = GetCellsFromRowString(rowData[0]).Length; //Split first row to figure out how many columns there should be
-
-        CSV csv = new CSV(columnCount, rowCount);
-
-        for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
+        public static CSV GenerateCSVFromFile(string fullPath)
         {
-            string currentRowData = rowData[rowIndex];
-            string[] rowCellContents = GetCellsFromRowString(currentRowData);
+            Debug.Log("Loading CSV from path " + fullPath);
+            if (!File.Exists(fullPath))
+                throw new System.Exception("File does not exist at path " + fullPath);
 
-            for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
-            {
-                string currentCellContents = rowCellContents[columnIndex];
-                csv.SetCellContents(columnIndex, rowIndex, currentCellContents);
-            }
+            FileStream fileStream = new FileStream(fullPath, FileMode.Open, FileAccess.ReadWrite);
+            StreamReader read = new StreamReader(fileStream);
+
+            string data = read.ReadToEnd();
+            read.Close();
+            fileStream.Close();
+            return GenerateCSVFromTextData(data);
         }
-        return csv;
-    }
+
+        public static CSV GenerateCSVFromTextData(string data)
+        {
+            Debug.Log("Populating CSV from data");
+
+            string[] rowData = Regex.Split(data, LINE_SPLIT_RE);
+
+            int rowCount = rowData.Length - 1;   //Skip over last row as it's created due to the final linebreak for end of file
+            int columnCount = GetCellsFromRowString(rowData[0]).Length; //Split first row to figure out how many columns there should be
+
+            CSV csv = new CSV(columnCount, rowCount);
+
+            for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
+            {
+                string currentRowData = rowData[rowIndex];
+                string[] rowCellContents = GetCellsFromRowString(currentRowData);
+
+                for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
+                {
+                    string currentCellContents = rowCellContents[columnIndex];
+                    csv.SetCellContents(columnIndex, rowIndex, currentCellContents);
+                }
+            }
+            return csv;
+        }
 
 
-    static string[] GetCellsFromRowString(string rowString)
-        => Regex.Split(rowString, SPLIT_RE);
+        static string[] GetCellsFromRowString(string rowString)
+            => Regex.Split(rowString, SPLIT_RE);
 
-    public static void SaveCSV(CSV csv, string fullPath)
-    {
-        StreamWriter writer = new StreamWriter(fullPath);
+        public static void SaveCSV(CSV csv, string fullPath)
+        {
+            StreamWriter writer = new StreamWriter(fullPath);
 
-        string contents = csv.GetContentsAsText();
-        writer.Write(contents);
+            string contents = csv.GetContentsAsText();
+            writer.Write(contents);
 
-        writer.Flush();
-        writer.Close();
-        AssetDatabase.Refresh();
+            writer.Flush();
+            writer.Close();
+            AssetDatabase.Refresh();
+        }
     }
 }
