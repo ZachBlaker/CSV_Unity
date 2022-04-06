4410a1f96b3edf7a39c5192db7c34d8884493daf
ZachBlaker <71710445+ZachBlaker@users.noreply.github.com>
2022-04-06 21:47:19 +0200

    Create CSVImporter.cs

---
+++ CSVImporter.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System.IO;
+
+public static class CSVImporter
+{
+    const string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+    const string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+
+    public static CSV GenerateCSVFromFile(string fullPath)
+    {
+        Debug.Log("Loading CSV from path " + fullPath);
+        if (!File.Exists(fullPath))
+            throw new System.Exception("File does not exist at path " + fullPath);
+
+        FileStream fileStream = new FileStream(fullPath, FileMode.Open, FileAccess.ReadWrite);
+        StreamReader read = new StreamReader(fileStream);
+
+        string data = read.ReadToEnd();
+        read.Close();
+        fileStream.Close();
+        return GenerateCSVFromTextData(data);
+    }
+
+    public static CSV GenerateCSVFromTextData(string data)
+    {
+        Debug.Log("Populating CSV from data");
+
+        string[] rowData = Regex.Split(data, LINE_SPLIT_RE);
+
+        int rowCount = rowData.Length -1;   //Skip over last row as it's created due to the final linebreak for end of file
+        int columnCount = GetCellsFromRowString(rowData[0]).Length; //Split first row to figure out how many columns there should be
+
+        CSV csv = new CSV(columnCount, rowCount);
+
+        for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
+        {
+            string currentRowData = rowData[rowIndex];
+            string[] rowCellContents = GetCellsFromRowString(currentRowData);
+
+            for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
+            {
+                string currentCellContents = rowCellContents[columnIndex];
+                csv.SetCellContents(columnIndex, rowIndex, currentCellContents);
+            }
+        }
+        return csv;
+    }
+
+
+    static string[] GetCellsFromRowString(string rowString)
+        => Regex.Split(rowString, SPLIT_RE);
+
+}
